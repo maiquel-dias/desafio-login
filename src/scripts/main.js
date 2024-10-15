01801f4f51f91af336de733f564531e0816065b9
maiquel-dias <maiquel.dias@db.tec.br>
2024-10-15 11:30:50 -0300

    Ajsute final

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,13 @@
-// Selecione os elementos do formulário
+
 const form = document.querySelector('.login-form');
 const emailInput = document.querySelector('#email');
 const senhaInput = document.querySelector('#senha');
 
-// Adicione um evento de submit ao formulário
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  // Valide o email e senha
+
   const email = emailInput.value.trim();
   const senha = senhaInput.value.trim();
 
